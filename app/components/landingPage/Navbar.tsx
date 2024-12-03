0598ea0e55e0c341b56a8d23dff8d4eef160fad3
Elias Soykat <eliasmd624@gmail.com>
2024-12-03 16:29:14 +0600

    fix : add website title name

--- app/components/landingPage/Navbar.tsx
+++ app/components/landingPage/Navbar.tsx
@@ -9,10 +9,10 @@ export function Navbar() {
     <div className="relative flex flex-col w-full py-5 mx-auto md:flex-row md:items-center md:justify-between">
       <div className="flex flex-row items-center justify-between text-sm lg:justify-start">
         <Link href="/" className="flex items-center gap-2">
-          <Image src={Logo} className="size-10" alt="Logo" />
+          <Image src={Logo} className="size-8" alt="Logo" />
 
           <h4 className="text-3xl font-semibold">
-            Cal<span className="text-primary">Marshal</span>
+            Schedulify
           </h4>
         </Link>
         <div className="md:hidden">
